33a0933adde8c3f9556752a105f1dd1abe72139a
KingstonBlaz <nounemeupharel@gmail.com>
2025-09-25 12:16:19 +0100

    Update test_tableau_trie.c

--- test_tableau_trie.c
+++ test_tableau_trie.c
@@ -1,26 +1,27 @@
 #include<stdio.h>
 
 void saisirtableau(int n,int v[n]){
-    printf("\n---Saisir tableau---\n");
+    printf("\n---Saisisez votre untableau---\n");
     for(int i=0;i<n;i++){
       printf("V[%d]=",i);
       scanf("%d",&v[i]);
     }
 }
 int main(){
-    int n,i,tmp;
+    int n,i,k;
     printf("Enter la dimension du tableau:");
     scanf("%d",&n);
-    printf("Entre les elements du tableau\n");
+    printf("Entre les elements( nombre entier )du tableau\n");
     int tab[n];
     saisirtableau(n,tab);
-    for(i=0;i<n-1;i++){
-    if(tab[i]>tab[i+1])
-    tmp=1;
+    for(i=0;i>n-1;i++){
+    if(tab[i]<tab[i+1])
+    k=1;
     else 
-    tmp=0;
+    k=0;
+        break;
     }
-    if(tmp==1) printf("Tableau non trie\n");
-    else printf("Tableau trie!\n");
+    if(k==0) printf(" les elements du tableau sont dans un  ordre non croissant\n");
+    else printf(" les elements du tableau sont dans l'orde croissant \n");
     return 0;
 }
